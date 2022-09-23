2ac4da7307f9be1bfcd9bf100f69d82d6be55dfb
Jym Bocala <jym.bocala@gmail.com>
2022-09-23 21:31:46 +1000

    fix .babelrc bug

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,12 @@
-// import { handleSubmit } from "./js/formHandler";
+import { handleSubmit } from "./js/formHandler";
 
-// import "./styles/resets.scss";
-// import "./styles/base.scss";
-// import "./styles/cards.scss";
-// import "./styles/header.scss";
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/cards.scss";
+import "./styles/header.scss";
 
-// export { handleSubmit };
+export { handleSubmit };
+
+console.log("CHANGE!!");
 
 alert("I EXIST");
