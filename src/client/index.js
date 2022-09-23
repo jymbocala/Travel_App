ef705f6064c26c22a614146bca2ac386135fd142
Jym Bocala <jym.bocala@gmail.com>
2022-09-23 19:22:24 +1000

    install webpack, webpack-cli & dev server

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { handleSubmit } from "./js/formHandler";
+
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/cards.scss";
+import "./styles/header.scss";
+
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,8 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+var path = require('path')
 const express = require('express');
-
-// Start up an instance of app
 const app = express();
 
 /* Dependencies */
@@ -19,18 +17,18 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('src/client'));
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('/client/html/index.html', { root: __dirname + '/..' })
+})
 
 // Setup Server
-const port = 3000;
-
-const server = app.listen(port, listening);
-
-function listening() {
-    console.log("server running");
-    console.log(`running on localhost: ${port}`);
-};
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
 
 //DATABASE
 const entryData = []
@@ -47,7 +45,6 @@ function getData (req, res) {
 app.post('/entries', addEntry);
 
 function addEntry(req,res){
-
     const newEntry = {
       date: req.body.date,
       temp: req.body.temp,
